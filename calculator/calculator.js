9b55a80e5095ef132f1a4770ad0f27ec23dd0784
Lily Comeau <133320631+lcomeau01@users.noreply.github.com>
2025-08-03 15:25:01 -0400

    Start creating calculator functionality

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,5 +1,42 @@
-const buttons[] = document.getElementsByClassName("button"); 
 
-buttons.array.forEach(element => {
-    
+
+const buttons = document.getElementsByClassName("button"); 
+console.log(buttons); 
+const calcWindow = document.getElementById("total"); 
+var currentPrev = ""; 
+var currentTotal = 0; 
+var currOperation = null; 
+
+Array.from(buttons).forEach(button => {
+    button.onclick = function() { 
+        let value = button.innerText; 
+        let classes = Array.from(button.classList); 
+        console.log(button.classList); 
+        if(classes.includes('number')) 
+        { 
+            currentPrev += value; 
+            currentTotal = parseFloat(currentPrev);
+        }
+        else if (classes.includes('action')) { 
+            let type = button.getAttribute('id'); 
+            if(type == 'clear') { 
+                currentTotal = 0; 
+                currentPrev = ""; 
+            } 
+            else if (type == 'scientific-notation') currentPrev += value; 
+            else if(type == 'equals')
+            { 
+                currentPrev = currentTotal; 
+            }
+        }
+        else 
+        { 
+            let type = button.getAttribute('id'); 
+            currOperation = type; 
+        }
+
+
+        if (currentPrev == "") calcWindow.innerText = "0"; 
+        else calcWindow.innerText = currentPrev; 
+    }
 });
