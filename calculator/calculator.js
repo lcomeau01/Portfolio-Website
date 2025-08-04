0029d69fe07e49ab37473360cbc21ef286b037a3
Lily Comeau <133320631+lcomeau01@users.noreply.github.com>
2025-08-03 21:32:12 -0400

    implement calculator functionality, rounding all numbers down to 9 digits at most, and displaying correct numbers on display screen

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,42 +1,83 @@
-
+import { dragElement } from "../drag.js";
+// calculator operation 
 
 const buttons = document.getElementsByClassName("button"); 
 console.log(buttons); 
 const calcWindow = document.getElementById("total"); 
-var currentPrev = ""; 
+var display = ""; 
 var currentTotal = 0; 
-var currOperation = null; 
+var currentEquation = ""; 
+let operation = false; 
+let clear = false; 
 
 Array.from(buttons).forEach(button => {
     button.onclick = function() { 
         let value = button.innerText; 
         let classes = Array.from(button.classList); 
-        console.log(button.classList); 
+        
         if(classes.includes('number')) 
         { 
-            currentPrev += value; 
-            currentTotal = parseFloat(currentPrev);
+            if(display == "0") display = value; 
+            else if (clear == true)
+            { 
+                display = value; 
+                currentEquation = ""; 
+                clear = false; 
+            }
+            else display += value; 
+
+            currentEquation += value;
         }
         else if (classes.includes('action')) { 
             let type = button.getAttribute('id'); 
             if(type == 'clear') { 
                 currentTotal = 0; 
-                currentPrev = ""; 
+                display = "0"; 
+                currentEquation = ""; 
             } 
-            else if (type == 'scientific-notation') currentPrev += value; 
-            else if(type == 'equals')
+            else if (type == 'scientific-notation') 
             { 
-                currentPrev = currentTotal; 
+                if(display == "0") display = "1" + value; 
+                else display += value; 
+
+                currentEquation += "1" + value;
+            } 
+            else if(type == 'equals')
+            {
+                operation = true;
+                clear = true;  
+                currentTotal = eval(currentEquation); 
+                currentEquation = currentTotal; 
+                display = currentTotal; 
             }
         }
         else 
         { 
-            let type = button.getAttribute('id'); 
-            currOperation = type; 
+            operation = true; 
+            if(clear == true) clear = false; 
+            currentTotal = eval(currentEquation); 
+            currentEquation = currentTotal + value; 
+            display = currentTotal; 
         }
 
-
-        if (currentPrev == "") calcWindow.innerText = "0"; 
-        else calcWindow.innerText = currentPrev; 
+        if(display == ".") calcWindow.innerText = "0."; 
+        else calcWindow.innerText = parseFloat(parseFloat(display).toPrecision(8)).toString(); 
+        
+        if(operation)
+        { 
+            display = ""; 
+            operation = false; 
+        } 
     }
 });
+
+// TODO: opening and closing the calculator 
+
+// TODO: moving the calculator around the screen 
+dragElement(document.getElementById("calculator-widget")); 
+
+
+
+
+
+
