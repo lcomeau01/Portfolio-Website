fab9ac9cad0d46357037a3f528e0dca93885c315
Lily Comeau <133320631+lcomeau01@users.noreply.github.com>
2025-08-14 00:49:59 -0400

    Add functionality to the date and time buttons, add draggability and closability to the widget
    Add functionality to the date and time buttons that invert the colors of the chosen button (to show you what you're currently on) and that change which display you're currently seeing. Beyond that I have added the functionality of a working clock to the second and an accurate representation of the current date when you look at the date display (checked only each time the button is clicked, not checked constantly like the clock)

--- alarm clock/alarm.js
+++ alarm clock/alarm.js
@@ -1,8 +1,82 @@
+// imported functions 
 import{ dragElement } from "../drag.js"; 
+import { closeWidget } from "../close.js"
 
+// const global variables 
 const alarm = document.getElementById("alarm-widget"); 
 const buttons = ['time', 'date', 'alarm']; 
+
+// default (global) values when the widget is first opened 
+var currentActive = document.getElementById('time'); 
+var display = document.getElementById("display-time"); 
+
+// actions to be performed on the widget 
 dragElement(alarm); 
+closeWidget(alarm);
+
+// display functions / information 
+startClock(document.getElementById("display-time")); 
+startCalendar(document.getElementById("date")); 
+
+
+buttons.forEach(function (elem) { 
+    let currButton = document.getElementById(elem); 
+    currButton.addEventListener("click", selectionChosen); 
+}); 
+
+function selectionChosen(event)
+{ 
+    // remove the active class from the last selection that had it 
+    currentActive.classList.remove('active'); 
+    display.classList.remove('active'); 
+
+    // add the active class to the new selection 
+    currentActive = event.currentTarget; 
+    display = document.getElementById("display-" + currentActive.id);
+    currentActive.classList.add('active'); 
+    display.classList.add('active'); 
+}
+
+/******* DISPLAY FUNCTIONS **********/
+
+function startClock(elem) {
+    
+    function updateTime() {
+        const currTime = new Date();
+        let hours = currTime.getHours();
+        const minutes = currTime.getMinutes().toString().padStart(2, "0");
+        const seconds = currTime.getSeconds().toString().padStart(2, "0");
+        const meridiem = hours >= 12 ? "PM" : "AM";
+
+        hours = hours % 12;
+        if (hours == 0) hours = 12;
+
+         elem.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem; 
+    }
+
+    updateTime(); // update immediately
+    return setInterval(updateTime, 1000); // update every second
+}
+
+function startCalendar(button)
+{ 
+    button.addEventListener("click", 
+                            getDate(document.getElementById("display-" + button.id))); 
+    
+    function getDate(elem)
+    { 
+        const currTime = new Date(); 
+        let day = currTime.getDate(); 
+        let month = currTime.getMonth() + 1; 
+        let year = currTime.getFullYear(); 
+
+        // console.log(currentTarget); 
+
+        elem.innerText = month + "/" + day + "/" + year; 
+    }
+}
+
+
 
 
 
