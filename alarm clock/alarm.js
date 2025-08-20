16e9be50c758fbbf48258210e7d1d0de21aa98c4
Lily Comeau <133320631+lcomeau01@users.noreply.github.com>
2025-08-19 20:25:06 -0400

    start creating alarm functionality
    create CSS changes when the input is going in and out of focus and allow user to type in numbers as well as backspaces to create alarm times

--- alarm clock/alarm.js
+++ alarm clock/alarm.js
@@ -81,9 +81,16 @@ function startAlarm()
 { 
     const alarmCheckbox = document.getElementById("alarm-switch"); 
     const alarmIcon = document.getElementById("alarm"); 
-    var alarmTimer, hour, minute, second, meridiem; 
+    var alarmTimer, alarmInputs, hour, minute, second, meridiem, focusedTime; 
 
     alarmCheckbox.addEventListener("click", alarmStateChanged); 
+    alarmInputs = document.getElementsByClassName('alarmTime'); 
+
+    Array.from(alarmInputs).forEach( (elem) => {
+        elem.addEventListener("focusin", changeAlarmFocus); 
+        elem.addEventListener("keydown", enteringTime); 
+        elem.addEventListener("focusout", verifyTime); 
+    }); 
 
     function alarmStateChanged()
     { 
@@ -91,6 +98,63 @@ function startAlarm()
         // and then unset alarm via clearTimeout 
         if(alarmCheckbox.checked) alarmIcon.classList.add("alarm-on"); 
         else alarmIcon.classList.remove("alarm-on"); 
+    }
+
+    function changeAlarmFocus(event)
+    { 
+        if (focusedTime != null) focusedTime.classList.remove("active");
+
+        focusedTime = event.currentTarget; 
+        focusedTime.classList.add("active"); 
+    
+    }
+
+    function enteringTime(event)
+    { 
+        if(focusedTime == null) return; 
+        console.log(event.key); 
+        let currTime = focusedTime.value; 
+        let key = event.key;
+
+        if(key == "Backspace") currTime = currTime.slice(0, -1);
+        else if (focusedTime.id == 'meridiem')
+        { 
+            if(key.toUpperCase() == "P" || key == "ArrowDown") focusedTime.value = "PM"; 
+            else if (key.toUpperCase() == "A" || key == "ArrowUp") focusedTime.value = "AM"; 
+            
+            return; 
+        }
+        else 
+        { 
+            key = parseInt(key); 
+            if(!Number.isInteger(key)) return; 
+            else currTime = currTime + key; 
+            currTime = currTime.slice(1, currTime.length); 
+        }
+
+        focusedTime.value = currTime.padStart(2, "0"); 
+    }
+
+    function verifyTime(event)
+    { 
+        let currTime = parseInt(focusedTime.value); 
+        
+        if(focusedTime.id == "meridiem") return; 
+        else if(focusedTime.id != hour)
+        { 
+            currTime = currTime % 60; 
+        }
+        else 
+        { 
+            currTime = currTime % 12; 
+            if(currTime == 0) currTime = 12; 
+        }
+        
+        focusedTime.value = currTime.toString().padStart(2, "0"); 
+
+        // there is no focused section of the alarm anymore 
+        focusedTime.classList.remove("active"); 
+        focusedTime = null; 
     }
 }
 
